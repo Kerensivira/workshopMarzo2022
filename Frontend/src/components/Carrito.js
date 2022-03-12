90ab11120f854e1a37f07ac5a46bde917ccd8e7d
Alexandra Sivira <kerensivira@gmail.com>
2022-03-11 23:42:28 -0500

    n

--- Frontend/src/components/Carrito.js
+++ Frontend/src/components/Carrito.js
@@ -1,4 +1,5 @@
 const agregarAlCarrito = document.querySelectorAll('#aggToBag')
+const CARDS = document.querySelector('#cardBox')
 
 agregarAlCarrito.forEach(botonCompra => {
     
@@ -13,6 +14,32 @@ const cards = button.closest('#container_CARDS')
 
 const cardTitle = document.querySelector('.card-title').textContent;
 const cardPrice = document.querySelector('.card-price').textContent;
-console.log(addToBagClicked = cardTitle, cardPrice);
+const cardimg = document.querySelector('.card-img-top').src;
+
+aggToBagCards(cardTitle, cardPrice, cardimg);
+
 
 }
+
+function aggToBagCards(cardTitle, cardPrice, cardimg){
+   const showMeCardsRow = document.createElement('div')
+   const cards_box = `
+   
+
+   <div id="cardBox">
+
+   <div class="item_card" style="width: 18rem;">
+       <img src= ${cardimg} class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">${cardTitle}</h5>
+         <p class="card-price"> ${cardPrice} </p>
+       
+       </div>
+     </div>
+
+</div>`;
+
+showMeCardsRow.innerHTML = cards_box
+CARDS.appendChild (showMeCardsRow);
+}
+
