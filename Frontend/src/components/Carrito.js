aa24cf6902c169b04743f935c407e9a73c92ada8
Alexandra Sivira <kerensivira@gmail.com>
2022-03-11 23:18:08 -0500

    carrito

--- Frontend/src/components/Carrito.js
+++ Frontend/src/components/Carrito.js
@@ -1,1 +1,18 @@
-document.querySelectorAll('.btn btn-warning')
+const agregarAlCarrito = document.querySelectorAll('#aggToBag')
+
+agregarAlCarrito.forEach(botonCompra => {
+    
+    botonCompra.addEventListener('click', addToBagClicked)
+
+});
+
+function addToBagClicked(event) {
+
+const button = event.target
+const cards = button.closest('#container_CARDS')
+
+const cardTitle = document.querySelector('.card-title').textContent;
+const cardPrice = document.querySelector('.card-price').textContent;
+console.log(addToBagClicked = cardTitle, cardPrice);
+
+}
