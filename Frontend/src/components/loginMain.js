74dd634f0175c5e46c3ba490e24b6b142ed36be4
Alexandra Sivira <kerensivira@gmail.com>
2022-03-11 20:56:57 -0500

    iuju

--- Frontend/src/components/loginMain.js
+++ Frontend/src/components/loginMain.js
@@ -2,6 +2,11 @@ const signupForm = document.querySelector('#survey-form');
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('submiting')
+    
+    const signupEmail = document.querySelector('#email');
+    const signupPassword = document.querySelector('#password');
+
+
+
 })
 
